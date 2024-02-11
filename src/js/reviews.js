4307339a0fde7e2b40768abcaefd4a0f9052d086
Vikto0004 <vikto0004@gmail.com>
2024-02-11 16:43:25 +0100

    modal

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,14 +1,30 @@
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  centeredSlides: true, // Центрувати активний слайд
-  loop: true, // Безкінечний цикл прокрутки
-  autoplay: {
-    delay: 500, // Затримка перед автоматичною прокруткою
-    disableOnInteraction: false, // Залишити автоматичну прокрутку після взаємодії користувача
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+// Ініціалізація Swiper
+let swiper = new Swiper('.reviews-description', {
+  effect: 'cards',
+  grabCursor: true,
 });
+
+// Функція для перевірки ширини екрану та вмикання/вимикання Swiper
+function toggleSwiper() {
+  if (window.innerWidth >= 768) {
+    // Вимикання Swiper якщо ширина екрану 768px або більше
+    if (swiper) {
+      swiper.destroy();
+      swiper = null;
+    }
+  } else {
+    // Увімкнення Swiper якщо ширина екрану менше 768px
+    if (!swiper) {
+      swiper = new Swiper('.reviews-description', {
+        effect: 'cards',
+        grabCursor: true,
+      });
+    }
+  }
+}
+
+// Викликаємо функцію для перевірки ширини екрану при завантаженні сторінки
+toggleSwiper();
+
+// Викликаємо функцію для перевірки ширини екрану при зміні розміру вікна
+window.addEventListener('resize', toggleSwiper);
